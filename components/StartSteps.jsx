096167a3fee466468a2ea379b56b7f621e1c9a88
Junaid Malik <junmalik109@gmail.com>
2022-12-12 19:54:17 +0000

    added line endings for vercel vm

--- components/StartSteps.jsx
+++ components/StartSteps.jsx
@@ -8,6 +8,16 @@ const StartSteps = ({ number, text }) => (
       <p className="font-bold text-[20px] text-white">
         {number}
       </p>
+      <button type="button" className="flex items-center h-fit py-4 px-6 bg-[#25618B] rounded-[32px] gap-[12px]">
+        <img
+          src="/headset.svg"
+          alt="headset"
+          className="w-[24px] h-[24px] object-contain"
+        />
+        <span className="font-normal text-[16px] text-white">
+          Enter Metaverse
+        </span>
+      </button>
     </div>
     <p className="flex-1 ml-[30px] font-normal text-[18px] text-[#B0B0B0] leading-[32.4px]">
       {text}
